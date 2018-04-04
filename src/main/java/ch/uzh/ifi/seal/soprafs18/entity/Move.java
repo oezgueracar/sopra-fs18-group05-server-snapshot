43ffedf4dc9011a120bacf83dc7d307433076ff2
lavillem <marc.laville@uzh.ch>
2018-04-04 15:42:59 +0200

    wrong branch, added BlockadeTest

--- src/main/java/ch/uzh/ifi/seal/soprafs18/entity/Move.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/entity/Move.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 public class Move implements Serializable {
 	
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	
--- src/test/java/ch/uzh/ifi/seal/soprafs18/entity/BlockadeTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs18/entity/BlockadeTest.java
@@ -1,24 +1,42 @@
 package ch.uzh.ifi.seal.soprafs18.entity;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BlockadeTest {
 
+    Space s1 = new Space(1, "green", 1, true);
+    Space s2 = new Space(2, "green", 1, true);
+    Space s3 = new Space(3, "green", 1, true);
+    Space s4 = new Space(4, "green", 1, true);
+
+    Blockade b1 = new Blockade(s1, s2, s3, s4, "green", 1);
+
     @Test
     public void getBlockadeLocation() {
+        Space[] blockade = {s1, s2, s3, s4};
+        assertArrayEquals(blockade, b1.getBlockadeLocation());
     }
 
     @Test
     public void getColor() {
+        assertEquals("green", b1.getColor());
     }
 
     @Test
     public void getValue() {
+        assertTrue(b1.getValue() == 1);
     }
 
     @Test
     public void removeBlockade() {
+        b1.removeBlockade();
+
+        assertTrue(b1.getBlockadeLocation()[0].getBlockade() == false);
+        assertTrue(b1.getBlockadeLocation()[1].getBlockade() == false);
+        assertTrue(b1.getBlockadeLocation()[2].getBlockade() == false);
+        assertTrue(b1.getBlockadeLocation()[3].getBlockade() == false);
+
+
     }
 }
