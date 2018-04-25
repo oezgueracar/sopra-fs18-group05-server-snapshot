b41140a7041a1e38859664d5f1d06e497d3fc3c4
oezgueracar <oezgueracar.gueler@uzh.ch>
2018-04-26 00:44:06 +0200

    Resolves #81

--- src/test/java/ch/uzh/ifi/seal/soprafs18/entity/PlayingPieceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs18/entity/PlayingPieceTest.java
@@ -1,16 +1,37 @@
 package ch.uzh.ifi.seal.soprafs18.entity;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PlayingPieceTest {
-	@Test
-	public void getColor() throws Exception {
+
+	Player p1;
+
+	@Before
+	public void setUp() throws Exception {
+		p1 = new Player();
+		p1.setColor("red");
+		p1.setup();
 	}
 
 	@Test
-	public void getPosition() throws Exception {
+	public void setPosition() {
+		p1.getPlayingPiece().setPosition(200);
+
+		assertTrue(p1.getPlayingPiece().getPosition() == 200);
 	}
 
+	@Test
+	public void getColor() {
+		assertEquals("red", p1.getPlayingPiece().getColor());
+	}
+
+	@Test
+	public void getPosition() {
+		p1.getPlayingPiece().setPosition(200);
+
+		assertTrue(p1.getPlayingPiece().getPosition() == 200);
+	}
 }
