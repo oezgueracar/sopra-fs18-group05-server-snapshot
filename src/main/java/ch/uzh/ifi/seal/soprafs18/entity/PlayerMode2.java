24296bf6382f287baca9b473aa863d0ab7ec2f34
oezgueracar <oezgueracar.gueler@uzh.ch>
2018-05-02 16:00:51 +0200

    Partly Resolves #114

--- src/main/java/ch/uzh/ifi/seal/soprafs18/entity/PlayerMode2.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/entity/PlayerMode2.java
@@ -1,18 +1,30 @@
 package ch.uzh.ifi.seal.soprafs18.entity;
 
-public class PlayerMode2 {
+public class PlayerMode2 extends Player {
 
 	private String colorPieceTwo;
 
 	private PlayingPiece assignedPiece2;
 
-    public PlayerMode2(String color1, String color2, String name, String token){
-
+    public PlayerMode2(String color, String name, String token){
+		super();
 	}
 
-	private String getColorPieceTwo(){
+	public void setColorPieceTwo(String colorPieceTwo) {
+		this.colorPieceTwo = colorPieceTwo;
+	}
 
+	public String getColorPieceTwo(){
 		return colorPieceTwo;
+	}
 
+	public PlayingPiece getAssignedPiece2(){
+    	return assignedPiece2;
+	}
+
+	@Override
+	public void setup(){
+    	super.setup();
+		this.assignedPiece = new PlayingPiece(this.color);
 	}
 }
