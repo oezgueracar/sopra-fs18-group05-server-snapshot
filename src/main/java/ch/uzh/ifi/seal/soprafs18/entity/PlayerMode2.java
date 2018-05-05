35c9c5cc91cb1cf85356caad0bbe399b5d205d1c
oezgueracar <oezgueracar.gueler@uzh.ch>
2018-05-06 01:03:04 +0200

    Enhanced PlayerMode2. #114

--- src/main/java/ch/uzh/ifi/seal/soprafs18/entity/PlayerMode2.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/entity/PlayerMode2.java
@@ -2,27 +2,10 @@ package ch.uzh.ifi.seal.soprafs18.entity;
 
 public class PlayerMode2 extends Player {
 
-	private String colorPieceTwo;
-
 	private PlayingPiece assignedPiece2;
 
-	// TODO: initialization of second playingPiece of a player - add parameter to recognize player.
-	/**
-	 * Constructor
-	 *
-	 * @param color1 color of the first playingPiece
-	 * @param color2 color of the second playingPiece
-	 */
     public PlayerMode2(){
 		super();
-	}
-
-	public void setColorPieceTwo(String colorPieceTwo) {
-		this.colorPieceTwo = colorPieceTwo;
-	}
-
-	public String getColorPieceTwo(){
-		return colorPieceTwo;
 	}
 
 	public PlayingPiece getAssignedPiece2(){
