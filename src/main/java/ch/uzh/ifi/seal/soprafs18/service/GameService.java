40e6b527d50b09343f43a420c71286835d32c62c
oezgueracar <oezgueracar.gueler@uzh.ch>
2018-05-11 20:09:54 +0200

    Reverted last commit

--- src/main/java/ch/uzh/ifi/seal/soprafs18/service/GameService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/service/GameService.java
@@ -448,7 +448,8 @@ public class GameService {
 						serverSidePlayer.get().getDiscardPile().add(serverSideGame.get().getMarket()
 											  .removeTransmitter(player.getBoughtCardId()));
 					}
-					else if (toBePlayedCard instanceof MulticolorCard) {
+					else if ((toBePlayedCard instanceof MulticolorCard)
+                            && player.getCardFromHandById(cardId) instanceof MulticolorCard) {
 						if (((MulticolorCard) player.getCardFromHandById(cardId)).getChosenColor() != null) {
 							((MulticolorCard) toBePlayedCard).setChosenColor(((MulticolorCard) player
 															 .getCardFromHandById(cardId)).getChosenColor());
