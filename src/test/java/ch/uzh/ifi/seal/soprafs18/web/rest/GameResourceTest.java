2620f38a8d41002ad3de4fbef042f7df539689ff
lavillem <marc.laville@uzh.ch>
2018-05-12 15:41:35 +0200

    werong comment

--- src/test/java/ch/uzh/ifi/seal/soprafs18/web/rest/GameResourceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs18/web/rest/GameResourceTest.java
@@ -108,8 +108,9 @@ public class GameResourceTest {
         mockMvc.perform(get("/games/1"))
                 .andExpect(status().isOk()).andExpect(content().string("{\"id\":1,\"name\":\"TestLeader's Game\",\"status\":\"ROOM\",\"currentPlayer\":0,\"turnTime\":60,\"mapName\":\"HillsOfGold\",\"assignedMap\":null,\"assignedMarket\":null,\"players\":[{\"id\":2,\"name\":\"TestLeaderCopy\",\"token\":\"" + userTokenString + "\",\"color\":\"blue\",\"playerLeft\":false,\"ready\":false,\"isInGoal\":false,\"gameId\":1,\"hand\":null,\"deck\":null,\"discardPile\":null,\"playedList\":null,\"moveCounter\":[0,0,0],\"playingPiece\":null}]}"));
 
-        //assertTrue(serverSideGame.getPlayers().size() == 1);*/
-    }
+        //assertTrue(serverSideGame.getPlayers().size() == 1);
+   } */
+
 
     @Test
     public void listGames() {
