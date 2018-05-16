df694d595425808c966df935e6531c07634d0b13
oezgueracar <oezgueracar.gueler@uzh.ch>
2018-05-16 21:33:08 +0200

    jenkins test

--- src/test/java/ch/uzh/ifi/seal/soprafs18/entity/AIntegrationTest/GameResourceTest1.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs18/entity/AIntegrationTest/GameResourceTest1.java
@@ -33,6 +33,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
@@ -50,6 +51,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = Application.class)
 @WebAppConfiguration
 @ActiveProfiles("integration")
+@Transactional
 //@WebAppConfiguration
 //@WebMvcTest(GameResource.class)
 //@SpringBootTest(classes = Application.class)
