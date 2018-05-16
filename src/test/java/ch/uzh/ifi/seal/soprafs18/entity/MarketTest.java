42250cd2dcd540af88fd0d09e18aa80b670d344a
lavillem <marc.laville@uzh.ch>
2018-05-16 15:18:20 +0200

    testfix

--- src/test/java/ch/uzh/ifi/seal/soprafs18/entity/MarketTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs18/entity/MarketTest.java
@@ -8,10 +8,12 @@ import static org.junit.Assert.*;
 public class MarketTest {
 
     Market m;
+    Market n;
 
     @Before
     public void setUp(){
         m = new Market();
+        n = new Market();
     }
 
     @Test
@@ -52,33 +54,33 @@ public class MarketTest {
 
     @Test
     public void removeTransmitter() {
-        for (int i = 0; i < m.getOpenSlots().size(); i++) {
+        for (int i = 0; i < n.getOpenSlots().size(); i++) {
 
-            assertEquals(3, m.getOpenSlots().get(i).length);
-            m.removeTransmitter(m.getOpenSlots().get(i)[0].getId());
-            assertEquals(2, m.getOpenSlots().get(i).length);
-            System.out.println("Oarray+" + i + " has" + m.getOpenSlots().get(i).length);
+            assertEquals(3, n.getOpenSlots().get(i).length);
+            n.removeTransmitter(n.getOpenSlots().get(i)[0].getId());
+            assertEquals(2, n.getOpenSlots().get(i).length);
+            System.out.println("Oarray+" + i + " has" + n.getOpenSlots().get(i).length);
 
-            m.removeTransmitter(m.getOpenSlots().get(i)[0].getId());
-            assertEquals(1, m.getOpenSlots().get(i).length);
-            System.out.println("Oarray+" + i + " has" + m.getOpenSlots().get(i).length);
+            n.removeTransmitter(n.getOpenSlots().get(i)[0].getId());
+            assertEquals(1, n.getOpenSlots().get(i).length);
+            System.out.println("Oarray+" + i + " has" + n.getOpenSlots().get(i).length);
 
-            m.removeTransmitter(m.getOpenSlots().get(i)[0].getId());
-            System.out.println("Oarray+" + i + " has" + m.getOpenSlots().get(i));
+            n.removeTransmitter(n.getOpenSlots().get(i)[0].getId());
+            System.out.println("Oarray+" + i + " has" + n.getOpenSlots().get(i));
 
         }
-        for(int i=0; i<m.getClosedSlots().size();i++){
-            System.out.println(m.getClosedSlots().size());
-            System.out.println("CArray+"+i+" has"+m.getClosedSlots().get(i).length);
-            assertEquals(3,m.getClosedSlots().get(i).length);
-            m.removeTransmitter(m.getClosedSlots().get(i)[0].getId());
-            System.out.println("Carray+"+i+" has"+m.getClosedSlots().get(i).length);
+        for(int i=0; i<n.getClosedSlots().size();i++){
+            System.out.println(n.getClosedSlots().size());
+            System.out.println("CArray+"+i+" has"+n.getClosedSlots().get(i).length);
+            assertEquals(3,n.getClosedSlots().get(i).length);
+            n.removeTransmitter(n.getClosedSlots().get(i)[0].getId());
+            System.out.println("Carray+"+i+" has"+n.getClosedSlots().get(i).length);
             assertEquals(2,m.getClosedSlots().get(i).length);
-            m.removeTransmitter(m.getClosedSlots().get(i)[0].getId());
-            System.out.println("Carray+"+i+" has"+m.getClosedSlots().get(i).length);
-            assertEquals(1,m.getClosedSlots().get(i).length);
-            m.removeTransmitter(m.getClosedSlots().get(i)[0].getId());
-            assertNull(m.getClosedSlots().get(i));
+            n.removeTransmitter(n.getClosedSlots().get(i)[0].getId());
+            System.out.println("Carray+"+i+" has"+n.getClosedSlots().get(i).length);
+            assertEquals(1,n.getClosedSlots().get(i).length);
+            n.removeTransmitter(n.getClosedSlots().get(i)[0].getId());
+            assertNull(n.getClosedSlots().get(i));
         }
     }
 
